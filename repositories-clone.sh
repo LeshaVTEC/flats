55f62617a757e87480772a4c7ff283645ec7e017
alexey <ivaldm90@gmail.com>
2024-01-14 19:09:12 +0300

    Add docker-compose.yaml and repositories-clone.sh

---
+++ repositories-clone.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git clone "https://github.com/LeshaVTEC/rent-config-service.git"
+git clone "https://github.com/LeshaVTEC/rent-audit-service.git"
+git clone "https://github.com/LeshaVTEC/rent-flats-service.git"
